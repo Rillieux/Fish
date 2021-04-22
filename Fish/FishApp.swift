ccdffd26d46f5f83163977c3c8ace4971f318fc1
Rillieux <dave.kondris@gmail.com>
2021-04-22 17:03:46 +0200

    Deleted ContentView template, removed some persistence.swift file code. Added MainTabView

--- Fish/FishApp.swift
+++ Fish/FishApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct FishApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainTabView()
         }
     }
 }
---
+++ Fish/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  Fish
+//
+//  Created by Dave Kondris on 22/04/21.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text("My Tabs")
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}

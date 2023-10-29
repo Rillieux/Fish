fbd7c5f2c93a785fdc91515c6c6ecad518ce0379
Rillieux <47659821+Rillieux@users.noreply.github.com>
2023-10-29 16:42:08 +0100

    Added @Observable macro for 17.0

---
+++ Fish/ObservableMacroView+Extension.swift
@@ -0,0 +1,11 @@
+//
+//  ObservableMacroView+Extension.swift
+//  Fish
+//
+//  Created at 16:30 on 29.10.2023.
+//
+//  Copyright (c) 2023  Dave Kondris. All rights reserved.
+//
+
+
+import Foundation
---
+++ Fish/ObservableMacroView.swift
@@ -0,0 +1,21 @@
+//
+//  ObservableMacroView.swift
+//  Fish
+//
+//  Created at 16:29 on 29.10.2023.
+//
+//  Copyright (c) 2023  Dave Kondris. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ObservableMacroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableMacroView()
+}
